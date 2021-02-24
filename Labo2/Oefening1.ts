cde2792b212352f9fc89a51c23a5b42b91b77f2c
Salih Komut <salih.komut@student.ap.be>
2021-02-24 17:00:31 +0100

    Oefening1 afgewerkt

--- Labo2/Oefening1.ts
+++ Labo2/Oefening1.ts
@@ -1,6 +1,21 @@
+let getallen: Array<number> = [];
+let getal: number = 0;
 
-let getallen: number[] = [];
-
-for (let i = 100; i > 200; i++){
+for (let i:number = 100; i > 200; i++){
 getallen.push(i);
+console.log(getallen.length);
 }
+
+while (getal < 100){
+    console.log(getallen[getal])
+    getal++;
+}
+
+/*for (let i = 0; i < 100; i++){
+    let nummer: number = getallen[i];
+    if (nummer % 3 == 0){
+        console.log(nummer);
+    }
+}*/
+
+
