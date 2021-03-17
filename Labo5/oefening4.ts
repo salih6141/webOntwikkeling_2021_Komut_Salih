4603eeddc69589be41b4a64bf0ac6cc187235d20
Salih Komut <salih.komut@student.ap.be>
2021-03-17 18:20:37 +0100

    Alle functies getest met console.log

--- Labo5/oefening4.ts
+++ Labo5/oefening4.ts
@@ -3,6 +3,7 @@ let calculateDogAge = (age:number):number => {return calculateAnimalAge(age,7)}
 let calculateAnimalAge = (ageAnimal:number, ageConverter:number) :number=>{
     return ageAnimal * ageConverter
 }
+console.log(calculateAnimalAge(1,10))
 
 let calculateAnimalAgeFunctional = (ageConverter:number) =>{
     return (age:number) => {return calculateAnimalAge(age, ageConverter)}
