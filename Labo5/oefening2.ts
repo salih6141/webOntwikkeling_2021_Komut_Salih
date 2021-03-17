98a1bdf9271d25eafde3e0b1b119297521bc5dc0
Salih Komut <salih.komut@student.ap.be>
2021-03-17 16:30:04 +0100

    opgave 2A getest en uitgevoerd

--- Labo5/oefening2.ts
+++ Labo5/oefening2.ts
@@ -1,8 +1,11 @@
 const printToConsole = (number: number) : void => console.log(`the result is ${number}`);
 
-let sum = (number: number, numbers: Array<number>) : void => {
+let sum = (numbers: Array<number>) : void => {
+    let number : number = 0;
     for (let i : number = 0; i < numbers.length;i++){
         number += numbers[i];
     }
     printToConsole(number);
 }
+
+sum([1,2,2,5,10]);
