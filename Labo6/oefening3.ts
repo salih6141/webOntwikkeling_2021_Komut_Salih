dd86bf114574e557538d270857c7a45aa39cf3cd
salih Komut <salih.komut@student.ap.be>
2021-04-13 17:09:12 +0200

    Oefening3.ts aangemaakt en voorbeeldcode gekopieerd

---
+++ Labo6/oefening3.ts
@@ -0,0 +1,17 @@
+interface Pokemon {
+    name: string
+    xp: number
+    type: string
+}
+
+let starterPokemonGen1 : Pokemon[] = [
+    {name: 'Bulbasaur', xp: 30, type: 'grass'},
+    {name: 'Charmander', xp: 50, type: 'fire'},
+    {name: 'Squirtle', xp: 45, type: 'water'}
+];
+let starterPokemonGen2 : Pokemon[]  = [
+    {name: 'Chikorita', xp: 30, type: 'grass'},
+    {name: 'Cyndaquil', xp: 50, type: 'fire'},
+    {name: 'Totodile', xp: 45, type: 'water'}
+];
+
