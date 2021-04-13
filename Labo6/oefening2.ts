2372df5829a16cfe2825e8779e896cef4ad299f4
salih Komut <salih.komut@student.ap.be>
2021-04-13 17:07:59 +0200

    capitalnames2 aangemaakt met de map functie in plaats van foreach en getest met console.log

--- Labo6/oefening2.ts
+++ Labo6/oefening2.ts
@@ -2,4 +2,7 @@ let names: string[] = ['joske', 'franske', 'donald', 'achmed']
 
 let capitalNames1: string[] = []
 names.forEach(e=>{capitalNames1.push(e.toUpperCase())})
-console.log(capitalNames1)
+//console.log(capitalNames1)
+
+let capitalNames2: string[] = names.map(e=>e.toUpperCase())
+console.log(capitalNames2)
