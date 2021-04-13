88b2216f03205a80e372b02ea3c3eabc88aeb804
salih Komut <salih.komut@student.ap.be>
2021-04-13 17:06:09 +0200

    array capitalnames for each toegevoegd om alles in drukletters over te nemen en getest met console.log

---
+++ Labo6/oefening2.ts
@@ -0,0 +1,5 @@
+let names: string[] = ['joske', 'franske', 'donald', 'achmed']
+
+let capitalNames1: string[] = []
+names.forEach(e=>{capitalNames1.push(e.toUpperCase())})
+console.log(capitalNames1)
