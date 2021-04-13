47286a36c2c544a8479c81d657ae2aef6ce6a660
salih Komut <salih.komut@student.ap.be>
2021-04-13 17:23:54 +0200

    Strongestpokemon toegevoegd

--- Labo6/oefening3.ts
+++ Labo6/oefening3.ts
@@ -27,4 +27,7 @@ let weakPokemonNames: string[] = starters.filter(p=>p.xp<40).map(p=>p.name)
 //console.log(weakPokemonNames)
 
 let sumOfAllXp: number = starters.reduce((sum,p)=>sum += p.xp,0)
-console.log(sumOfAllXp)
+//console.log(sumOfAllXp)
+
+let strongestPokemon: Pokemon = starters.reduce((currentStrongestPokemon, pokemon)=>currentStrongestPokemon.xp>pokemon.xp ? currentStrongestPokemon: pokemon,{name:'', xp:-1000,type:''})
+//console.log(strongestPokemon)
