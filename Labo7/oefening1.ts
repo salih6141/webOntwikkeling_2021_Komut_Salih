1d6172fcd8b6c6dbcb9b9fc02daa82bfc2830a7c
salih Komut <salih.komut@student.ap.be>
2021-04-25 19:23:25 +0200

    labo7 configuratie en start

---
+++ Labo7/oefening1.ts
@@ -0,0 +1,15 @@
+const slowSum = (a: number, b: number) => {
+    return new Promise<number>((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a+b);
+        },1000)
+    });
+}
+
+const slowMult = (a: number, b: number) => {
+    return new Promise<number>((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a*b);
+        },1500)
+    });
+}
