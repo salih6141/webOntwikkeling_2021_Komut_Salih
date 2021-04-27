27b1b689cf81bf06dd82c53dcaf038b149a6a5f6
Salih Komut <s117881@ap.be>
2021-04-27 16:14:49 +0200

    een simpeleApi hoofdstuk getest

---
+++ theorieweek9/simpeleApi.ts
@@ -0,0 +1,25 @@
+const express1 = require('express');
+const app1 = express1();
+app1.set('port', 3000);
+
+const data = [
+    {
+        name:'goerge',
+        age: 50
+    },
+    {
+        name:'jane',
+        age: 32
+    },
+    {
+        name:'john',
+        age: 42
+    },
+];
+
+app1.get('/getData',(req:any,res:any)=>{
+    res.type('application/json');
+    res.json(data);
+})
+
+app1.listen(app1.get('port'), ()=>console.log('[server] http://localhost:' + app.get('port')));
