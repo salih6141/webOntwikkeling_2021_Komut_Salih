c362d11e5cc9425349ff050e829e1ddab80e32c9
Salih Komut <s117881@ap.be>
2021-04-27 02:33:56 +0200

    eerste uitwerking fetch cocktail

---
+++ Labo7/oefening2.ts
@@ -0,0 +1,10 @@
+import { promises } from "node:stream";
+
+const fetch = require('node fetch');
+
+let promise1 = fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11000').then((Response:any) => Response.json());
+let promise2 = fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11001').then((Response:any) => Response.json());
+let promise3 = fetch('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11002').then((Response:any) => Response.json());
+Promise.all([promise1,promise2,promise3]).then((cocktails) =>{
+    console.log(cocktails[0].name, cocktails[1].name, cocktails[2].name);
+})
