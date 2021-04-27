fdd6ca78fafbff9132b0340ef5b135cae88bbad1
Salih Komut <s117881@ap.be>
2021-04-27 15:19:50 +0200

    Labo7 afgewerkt en getest

--- Labo7/oefening3.ts
+++ Labo7/oefening3.ts
@@ -2,7 +2,7 @@ const fetch2 = require('node-fetch')
 
 const dadJokes = async() => {
   for (let i:number = 1; i <= 3; i++){
-    let jokes = await fetch('https://icanhazdadjoke.com/search?term=dog&limit=5&page=1', {
+    let jokes = await fetch2('https://icanhazdadjoke.com/search?term=dog&limit=5&page=1', {
       headers: {
         'Accept': 'application/json'
       }
@@ -13,4 +13,5 @@ const dadJokes = async() => {
    }
   }
 }
-
+dadJokes();
+export{};
