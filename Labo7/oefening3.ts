d3b6fdf6a0450ed6fead8612f2a3d2a640d548bc
Salih Komut <s117881@ap.be>
2021-04-27 02:50:32 +0200

    oefening3.ts test (nog niet volledig afgewerkt)

---
+++ Labo7/oefening3.ts
@@ -0,0 +1,9 @@
+fetch('https://icanhazdadjoke.com/search?term=dog&limit=5&page=1', {
+   headers: {
+     'Accept': 'application/json'
+   }
+})
+
+const alleenDog = async (joke:string):Promise<void> => {
+    if 
+}
