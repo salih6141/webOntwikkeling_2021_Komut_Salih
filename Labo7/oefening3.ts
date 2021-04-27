aebf28c15190acd929cd197d14b37203b3da6a44
Salih Komut <s117881@ap.be>
2021-04-27 15:11:50 +0200

    for loop en api fetch'

--- Labo7/oefening3.ts
+++ Labo7/oefening3.ts
@@ -1,10 +1,14 @@
 const fetch2 = require('node-fetch')
 
-fetch('https://icanhazdadjoke.com/search?term=dog&limit=5&page=1', {
-   headers: {
-     'Accept': 'application/json'
-   }
-})
+const dadJokes = async() => {
+  for (let i:number = 1; i <= 3; i++){
+    let jokes = await fetch('https://icanhazdadjoke.com/search?term=dog&limit=5&page=1', {
+      headers: {
+        'Accept': 'application/json'
+      }
+   })
+  }
+}
 
 const alleenDog = async (joke:string):Promise<void> => {
     if 
