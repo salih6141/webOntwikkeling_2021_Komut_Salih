f9c697dee999cbec4b89788ef44ad65e48caf4b3
Salih Komut <s117881@ap.be>
2021-04-28 00:02:01 +0200

    alle andere url's toegevoegd

--- labo8/index.ts
+++ labo8/index.ts
@@ -12,5 +12,20 @@ app2.get('/whoami',(req:any,res:any)=>{
     res.send('Hello World, whoami')
 })
 
+app2.get('/whoamijson',(req:any,res:any)=>{
+    res.type('text/html');
+    res.send('Hello World, whoamijson')
+})
+
+app2.get('/pikachujson',(req:any,res:any)=>{
+    res.type('text/html');
+    res.send('Hello World, pikachu')
+})
+
+app2.get('/pikachuhtml',(req:any,res:any)=>{
+    res.type('text/html');
+    res.send('Hello World, pikachuhtml')
+})
+
 app2.listen(app2.get('port'),
     ()=>console.log('[server] http://localhost:' + app2.get('port')));
