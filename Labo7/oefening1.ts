25d970604c4bcb4925e47cb014161cd6561e9dfd
Salih Komut <s117881@ap.be>
2021-04-27 02:09:14 +0200

    oefening 1 volledig afgewerkt

--- Labo7/oefening1.ts
+++ Labo7/oefening1.ts
@@ -1,3 +1,7 @@
+import { rejects } from "assert/strict";
+import { resolve } from "node:path";
+import { promises } from "node:stream";
+
 const slowSum = (a: number, b: number) => {
     return new Promise<number>((resolve, reject) => {
         setTimeout(() => {
@@ -14,4 +18,36 @@ const slowMult = (a: number, b: number) => {
     });
 }
 
-console.log(slowSum(1,5))
+const slowDiv = (a:number, b:number) => {
+    return new Promise<number>((resolve,reject) => {
+        setTimeout(() => {
+            if (b !== 0){
+            resolve(a/b);
+            }
+            else{
+            reject("You cannot divide by zero");
+            }
+        }, 2000);
+    })
+}
+
+// slowSum(1,5).then((value)=>{
+//     console.log(value)
+//     slowMult(value,2).then((uitkomst)=>{
+//         console.log(uitkomst)
+//         slowDiv(uitkomst,2).then((deling)=>{
+//             console.log(deling)
+//         })
+//     })
+// })
+
+// slowDiv(6,3).then((value)=>{
+//     console.log(value)
+// })
+
+slowDiv(3,0).then((value)=>{
+    console.log(value)
+})
+.catch(err =>{
+    console.log(err)
+})
