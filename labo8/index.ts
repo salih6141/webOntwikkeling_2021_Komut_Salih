4cdabaf80b4c91647e95a8920ab81f51bca85eb0
Salih Komut <s117881@ap.be>
2021-04-27 23:51:54 +0200

    eerste url aangemaakt

--- labo8/index.ts
+++ labo8/index.ts
@@ -1,3 +1,8 @@
 const express2 = require('express');
 const app2 = express();
 app2.set('port',3000);
+
+app2.get('/',(req:any,res:any)=>{
+    res.type('text/html');
+    res.send('Hello world')
+})
