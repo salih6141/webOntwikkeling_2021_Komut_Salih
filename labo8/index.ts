3258f79358c0fb4bdf7c9a48ea0b0f206693f3fc
Salih Komut <s117881@ap.be>
2021-04-28 20:02:03 +0200

    bij de juiste pagina de variabelen meegegeven

--- labo8/index.ts
+++ labo8/index.ts
@@ -8,17 +8,16 @@ app2.set('view engine', 'ejs');
 app2.set('port',3000);
 
 app2.get('/',(req:any,res:any)=>{
-    res.render('index',{
-        name: 'komut salih',
-        age: 22,
-        profilepic: 'https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&h=661&q=70&fm=webp'
-    });
+    res.render('index')
 })
 
 app2.get('/whoami',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello World, whoami')
-})
+    res.render('whoami',{
+        name: 'komut salih',
+        age: 22,
+        profilepic: 'https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&h=661&q=70&fm=webp'
+    })
+});
 
 app2.get('/whoamijson',(req:any,res:any)=>{
     res.type('text/html');
