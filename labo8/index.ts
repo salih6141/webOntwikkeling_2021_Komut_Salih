e9ca730c9cb6f1e67f9d6de9be5057c14daf0bb7
Salih Komut <s117881@ap.be>
2021-04-28 19:51:41 +0200

    variabele gedeclareerd en waarden meegegeven met res.render en die waarden gebruikt in ejs file

--- labo8/index.ts
+++ labo8/index.ts
@@ -1,3 +1,5 @@
+import { Profiler, url } from "node:inspector";
+
 const express3 = require('express')
 const app2 = express3();
 const ejs = require('ejs');
@@ -6,28 +8,32 @@ app2.set('view engine', 'ejs');
 app2.set('port',3000);
 
 app2.get('/',(req:any,res:any)=>{
-    res.render('index');
+    res.render('index',{
+        name: 'komut salih',
+        age: 22,
+        profilepic: 'https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&h=661&q=70&fm=webp'
+    });
 })
 
-// app2.get('/whoami',(req:any,res:any)=>{
-//     res.type('text/html');
-//     res.send('Hello World, whoami')
-// })
+app2.get('/whoami',(req:any,res:any)=>{
+    res.type('text/html');
+    res.send('Hello World, whoami')
+})
 
-// app2.get('/whoamijson',(req:any,res:any)=>{
-//     res.type('text/html');
-//     res.send('Hello World, whoamijson')
-// })
+app2.get('/whoamijson',(req:any,res:any)=>{
+    res.type('text/html');
+    res.send('Hello World, whoamijson')
+})
 
-// app2.get('/pikachujson',(req:any,res:any)=>{
-//     res.type('text/html');
-//     res.send('Hello World, pikachu')
-// })
+app2.get('/pikachujson',(req:any,res:any)=>{
+    res.type('text/html');
+    res.send('Hello World, pikachu')
+})
 
-// app2.get('/pikachuhtml',(req:any,res:any)=>{
-//     res.type('text/html');
-//     res.send('Hello World, pikachuhtml')
-// })
+app2.get('/pikachuhtml',(req:any,res:any)=>{
+    res.type('text/html');
+    res.send('Hello World, pikachuhtml')
+})
 
 app2.listen(app2.get('port'),
     ()=>console.log('[server] http://localhost:' + app2.get('port')));
