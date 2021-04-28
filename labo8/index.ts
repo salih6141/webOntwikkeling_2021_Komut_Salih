89d1c57a19e113344e7b0ea651fe74e9d03293cf
salih Komut <salih.komut@student.ap.be>
2021-04-28 19:10:05 +0200

    de'views' folder op de juiste plaats gezet samen met de bijhorende ejs file

--- labo8/index.ts
+++ labo8/index.ts
@@ -2,32 +2,32 @@ const express3 = require('express')
 const app2 = express3();
 const ejs = require('ejs');
 
+app2.set('view engine', 'ejs');
 app2.set('port',3000);
-app2.set('view engine','ejs');
 
 app2.get('/',(req:any,res:any)=>{
     res.render('index');
 })
 
-app2.get('/whoami',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello World, whoami')
-})
+// app2.get('/whoami',(req:any,res:any)=>{
+//     res.type('text/html');
+//     res.send('Hello World, whoami')
+// })
 
-app2.get('/whoamijson',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello World, whoamijson')
-})
+// app2.get('/whoamijson',(req:any,res:any)=>{
+//     res.type('text/html');
+//     res.send('Hello World, whoamijson')
+// })
 
-app2.get('/pikachujson',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello World, pikachu')
-})
+// app2.get('/pikachujson',(req:any,res:any)=>{
+//     res.type('text/html');
+//     res.send('Hello World, pikachu')
+// })
 
-app2.get('/pikachuhtml',(req:any,res:any)=>{
-    res.type('text/html');
-    res.send('Hello World, pikachuhtml')
-})
+// app2.get('/pikachuhtml',(req:any,res:any)=>{
+//     res.type('text/html');
+//     res.send('Hello World, pikachuhtml')
+// })
 
 app2.listen(app2.get('port'),
     ()=>console.log('[server] http://localhost:' + app2.get('port')));
