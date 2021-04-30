3eaa8fc50d97b38ad47178040a5c7f7745a57d2d
salih Komut <salih.komut@student.ap.be>
2021-04-30 18:56:20 +0200

    bestanden doorgestuurd als json en getoond op url '/whoamisjon'

--- labo8/index.ts
+++ labo8/index.ts
@@ -22,7 +22,9 @@ app2.get('/whoami',(req:any,res:any)=>{
 
 app2.get('/whoamijson',(req:any,res:any)=>{
     res.render('whoamijson',{
-        
+        "name" : "komut salih",
+        "age" : 22,
+        "profilepic" : "https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&h=661&q=70&fm=webp"
     })
 })
 
